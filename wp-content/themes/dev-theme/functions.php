d12d45f51d0b1cd5b191f19fd97dfa35184ae4b3
Levi Zitting <levizitting5663@gmail.com>
2017-04-20 13:55:32 -0500

    Starting to build theme

--- wp-content/themes/dev-theme/functions.php
+++ wp-content/themes/dev-theme/functions.php
@@ -71,6 +71,61 @@ function dev_theme_setup() {
 endif;
 add_action( 'after_setup_theme', 'dev_theme_setup' );
 
+function dev_theme_fonts_url() {
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Sourcs Sans Pro font: on or off', 'dev-theme' );
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'dev-theme' );
+
+	$font_families = array();
+
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,600,900';
+ 	}
+
+	if ( 'off' !== $pt_serif) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+ 	}
+ 
+	if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Dev-Theme 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function dev_theme_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'dev-theme-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls; 
+}
+add_filter( 'wp_resource_hints', 'dev_theme_resource_hints', 10, 2 );
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -105,6 +160,9 @@ add_action( 'widgets_init', 'dev_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function dev_theme_scripts() {
+	// Enqueue Google Fonts: Source Sans Pro and PT Serif
+	wp_enqueue_style('dev-themes-fonts', dev_theme_fonts_url());
+
 	wp_enqueue_style( 'dev-theme-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'dev-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
