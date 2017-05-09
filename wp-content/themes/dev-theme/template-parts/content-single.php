8d41a11d2b4710b68a68e36a313772abc7707427
Levi Zitting <levizitting5663@gmail.com>
2017-05-09 17:15:14 -0500

    Updating

--- wp-content/themes/dev-theme/template-parts/content-single.php
+++ wp-content/themes/dev-theme/template-parts/content-single.php
@@ -27,6 +27,13 @@
 		endif; ?>
 	</header><!-- .entry-header -->
 
+    <?php 
+    if ( has_post_thumbnail() ) { ?>
+    <figure class="featured-image full-bleed">
+        <?php the_post_thumbnail("dev-theme-full-bleed"); ?>
+    </figure>
+    <?php } ?>
+
     <section class="post-content">
 
         <?php
